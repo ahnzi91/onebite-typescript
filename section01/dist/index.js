46c044bd50853da2adaede452a25eb5b442e4e4d
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2025-02-06 20:40:20 +0900

    ✨feat: 타입스크립트 컴파일러 옵션 설정하기 수강 완료

--- section01/dist/index.js
+++ section01/dist/index.js
@@ -1,12 +1,9 @@
 // import { hello } from "./hello";
-
 // console.log("Hello TypeScript");
 // const a: number = 1;
-
 // const func = () => {
 //   console.log("Hello");
 // };
-
 // hello("Ahnzi");
-
 const a = 1;
+export {};
