2b4456cf158680cb54738ddfd3b9aa679ef44472
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2025-02-21 11:16:14 +0900

    ✨feat: Enum 타입 수강 완료

--- section01/src/index.ts
+++ section01/src/index.ts
@@ -1,12 +1,2 @@
-// import { hello } from "./hello";
-
-// console.log("Hello TypeScript");
-// const a: number = 1;
-
-// const func = () => {
-//   console.log("Hello");
-// };
-
-// hello("Ahnzi");
-
-const a = 1;
+console.log("Hello TypeScript");
+const a: number = 1;
